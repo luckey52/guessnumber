794e2dbd9bbd55e61aa7379cf03ec52e35df3afc
KB Choi <1mdifferent@naver.com>
2024-02-12 02:25:19 +0900

    2nd commit

--- main.js
+++ main.js
@@ -12,8 +12,17 @@ let compumterNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
 
 function pickRandomNum() {
   compumterNum = Math.floor(Math.random() * 100) + 1;
@@ -21,12 +30,37 @@ function pickRandomNum() {
 }
 function play() {
   let userValue = userInput.value;
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1과100사이 숫자를 입력해주세요";
+    return;
+  }
+  if (history.includes(userValue)) {
+    resultArea.textContent = "이미 입력한 숫자입니다";
+    return;
+  }
+  chances--;
+  chanceArea.textContent = `남은기회:${chances}번`;
+  console.log("chances", chances);
   if (userValue < compumterNum) {
     resultArea.textContent = "Up!!";
   } else if (userValue > compumterNum) {
     resultArea.textContent = "Down!!";
   } else {
     resultArea.textContent = "You're Correct!!";
+    gameOver = true;
   }
+  history.push(userValue);
+  console.log(history);
+  if (chances < 1) {
+    gameOver = true;
+  }
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+function reset() {
+  userInput.value = "";
+  pickRandomNum();
+  resultArea.textContent = "결과값이 여기에 나옵니다";
 }
 pickRandomNum();
